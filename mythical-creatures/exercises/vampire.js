d3a046deec3e86f19cc50a6ed02f2ad320dfe834
Mark Yen <77414433+markcyen@users.noreply.github.com>
2022-01-04 13:13:40 -0700

    Complete implementing vampire class

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,20 @@
+class Vampire {
+  constructor(name, pet = `bat`) {
+    this.name = name;
+    this.pet = pet;
+    this.thirsty = true;
+    this.ouncesDrank = 0;
+  }
+
+  drink() {
+    this.thirsty = false;
+
+    if (this.ouncesDrank >= 50) {
+      return `I'm too full to drink anymore!`
+    } else {
+      this.ouncesDrank += 10;
+    }
+  }
+}
+
+module.exports = Vampire;
